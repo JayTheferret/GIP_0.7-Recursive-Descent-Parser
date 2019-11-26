ccab5d2e48b3b08f9baa359b053720b6707c374d
Jay <57915168+JayTheferret@users.noreply.github.com>
2019-11-26 18:48:09 +0100

    Update match_expect.cpp

--- match_expect.cpp
+++ match_expect.cpp
@@ -4,38 +4,39 @@
 
 using namespace std;
 
+//weiter springen, fehler nennen
+
 void match(char c,string& input, int& pos)
 {
 	cout << "Betrete match() fuer das Zeichen " << c << endl;
 
-	if (unsigned(pos) >= input.length()) //wenn position größer als Input
+	if (unsigned(pos) >= input.length()) //wenn Position größer als Input
 	{
 		cout << "Input-Zeichenkette zu kurz. "
 			<< "Erwarte noch das Zeichen " << c << endl;
 		cout << "Verlasse match(): " << endl;
+		return;
 	}
 
 	if (input.at(pos) != c)
 	{
-		cout << "Fehler: an Position "
-			<< pos << " erwarte "
-			<< c << " und sehe " << input.at(pos) << endl;
+		cout << "Fehler: an Position " << pos << " erwarte " << c << " und sehe " << input.at(pos) << endl;
 		cout << "Verlasse match(): " << endl;
-
+		return;
 	}
-	pos++; //eine position weitergehen
+	pos++; //eine Position weitergehen
 
 	cout << "Zeichen " << c << " konsumiert." << endl;
-	cout << "Verlasse match() fuer das Zeichen "
-		<< c << endl;
+	cout << "Verlasse match() fuer das Zeichen " << c << endl;
 }
+
 
 bool expect(char c, string& input, int pos)
 {
 	cout << "Teste auf das Zeichen " << c << endl;
 	if (unsigned(pos) >= input.length()) //nach ende
 	{
-		cout << "Aber schon am Ende der Eingabe-Zeichenkette angelangt." << endl ; //damit kein U/O etc am ende-> nur ziffern
+		cout << "Aber schon am Ende der Eingabe-Zeichenkette angelangt." << endl ; 
 		return false; 
 	}
 	if (input.at(pos) == c) //zeichen gefunden
